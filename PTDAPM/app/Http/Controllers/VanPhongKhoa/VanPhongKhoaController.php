f698b16246ac5b18e59da7df594ac1640c4e1fd8
mchien1005 <167665286+mchien1005@users.noreply.github.com>
2025-03-11 18:10:23 +0700

    layouts

---
+++ PTDAPM/app/Http/Controllers/VanPhongKhoa/VanPhongKhoaController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\VanPhongKhoa;
+
+use App\Models\Khoa;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class VanPhongKhoaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function truyVanThongTin()
+    {
+        return view('vanphongkhoa.truyvanthongtin');
+    }
+    public function capnhatketqua()
+    {
+        return view('vanphongkhoa.capnhatketqua');
+    }
+    public function index()
+    {
+        return view('vanphongkhoa.index');
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Khoa $khoa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Khoa $khoa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Khoa $khoa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Khoa $khoa)
+    {
+        //
+    }
+}
