b7ca0e699a17e7cc45b3f8fae729157fa5396b36
mchien1005 <167665286+mchien1005@users.noreply.github.com>
2025-03-11 15:51:35 +0700

    cập nhật layouts

--- PTDAPM/routes/web.php
+++ PTDAPM/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SinhVien\.....;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// gõ route ở đây
+
+use App\Http\Controllers\GiangVien\......;
+
+// gõ route ở đây
+
+use App\Http\Controllers\VanPhongkhoa\......;
+
+// gõ route ở đây
+
+use App\Http\Controllers\PhongDaoTao\......;
+
+// gõ route ở đây
