050cae8cfe73604101bbf8ed8bea679a2cbdb629
mchien1005 <167665286+mchien1005@users.noreply.github.com>
2025-03-14 00:47:03 +0700

    Update VPKroute.php

--- PTDAPM/routes/VPKroute.php
+++ PTDAPM/routes/VPKroute.php
@@ -4,7 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaiKhoan\LoginController;
 use App\Http\Controllers\VanPhongKhoa\VanPhongKhoaController;
 
+Route::get('/', function () {
+    return redirect()->route('login.form');
+});
 
+Route::get('/dang-nhap', [LoginController::class, 'index'])->name('login.form'); // Trang hiển thị form đăng nhập
+Route::post('/dang-nhap', [LoginController::class, 'login'])->name('login.process');
 Route::middleware(['auth'])->group(function () {
     Route::prefix('vanphongkhoa')->group(function () {
         Route::get('/capnhatketqua', [VanPhongKhoaController::class, 'capnhatketqua'])->name('vanphongkhoa.capnhatketqua');
