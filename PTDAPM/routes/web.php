b05cf731149311a19e86cd71d9a6d445a810ff3b
mchien1005 <167665286+mchien1005@users.noreply.github.com>
2025-03-13 21:17:54 +0700

    Bố cục mới

--- PTDAPM/routes/web.php
+++ PTDAPM/routes/web.php
@@ -1,18 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SinhVien\.....;
-
-// gõ route ở đây
-
-use App\Http\Controllers\GiangVien\......;
-
-// gõ route ở đây
-
-use App\Http\Controllers\VanPhongkhoa\......;
-
-// gõ route ở đây
-
-use App\Http\Controllers\PhongDaoTao\......;
-
-// gõ route ở đây
