ad7265c8566cd180943ef4754605fe3848fb60e6
mchien1005 <167665286+mchien1005@users.noreply.github.com>
2025-03-13 23:45:31 +0700

    tách route

--- PTDAPM/routes/VPKroute.php
+++ PTDAPM/routes/VPKroute.php
@@ -4,13 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaiKhoan\LoginController;
 use App\Http\Controllers\VanPhongKhoa\VanPhongKhoaController;
 
-Route::get('/', function () {
-    return redirect()->route('login.form');
-});
-
-Route::get('/dang-nhap', [LoginController::class, 'index'])->name('login.form'); // Trang hiển thị form đăng nhập
-Route::post('/dang-nhap', [LoginController::class, 'login'])->name('login.process'); // Xử lý đăng nhập
-
 
 Route::middleware(['auth'])->group(function () {
     Route::prefix('vanphongkhoa')->group(function () {
@@ -23,6 +16,3 @@ Route::middleware(['auth'])->group(function () {
         Route::post('/dang-xuat', [LoginController::class, 'logout'])->name('logout');
     });
 });
-
-
-require base_path('routes/VPKroute.php');
