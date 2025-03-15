ed9ec97dec61baf5745b43596129a2536311d6be
mchien1005 <167665286+mchien1005@users.noreply.github.com>
2025-03-15 13:39:14 +0700

    cập nhật các route ở sidebar

--- PTDAPM/resources/views/FormVanPhongKhoa/FormTinTuc/FormTinTuc.blade.php
+++ PTDAPM/resources/views/FormVanPhongKhoa/FormTinTuc/FormTinTuc.blade.php
@@ -3,13 +3,14 @@
 @section('sidebar')
     <div class="menu-item active">
         <a href=" {{ route('vanphongkhoa.tintuc') }}"">
-                                <img src=" {{ asset('images/Commercial.png') }}" alt="News Icon" class="sidebar-icon" />
+                                    <img src=" {{ asset('images/Commercial.png') }}" alt="News Icon"
+            class="sidebar-icon" />
         <span>Tin tức</span>
         </a>
     </div>
     <div class="menu-item">
         <a href=" {{ route('vanphongkhoa.xembaocao') }}"">
-                                <img src=" {{ asset('images/School Director.png') }}" alt="Research Icon"
+                                    <img src=" {{ asset('images/School Director.png') }}" alt="Research Icon"
             class="sidebar-icon" />
         <span>Báo cáo đề tài</span>
         </a>
