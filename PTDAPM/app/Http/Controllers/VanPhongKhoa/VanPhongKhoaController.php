4f69154bcde060793e2c30875097f64d4fc23d83
cr7coder <167843477+cr7coder@users.noreply.github.com>
2025-03-16 18:22:12 +0700

    Update VanPhongKhoaController.php

--- PTDAPM/app/Http/Controllers/VanPhongKhoa/VanPhongKhoaController.php
+++ PTDAPM/app/Http/Controllers/VanPhongKhoa/VanPhongKhoaController.php
@@ -31,33 +31,55 @@ class VanPhongKhoaController extends Controller
     {
         return view('FormVanPhongKhoa.FormQuanLyPhanBien.FormGhepDoiDeTai-HoiDong');
     }
-    public function xemBaoCao()
-    {
-        return view('FormVanPhongKhoa.FormQuanLyBaoCaoNghienCuu.FormXemBaoCao');
+   public function xemBaoCao(){
+    $baocaos = BaoCaoNghienCuu::all();
+    return view('FormVanPhongKhoa.FormQuanLyBaoCaoNghienCuu.FormXemBaoCao', compact('baocaos'));
     }
-    public function phanBienVaBaoVe()
-    {
+
+    public function showBaoCao($ma_bc){
+    // Lấy dữ liệu báo cáo từ database
+    $baocao = BaoCaoNghienCuu::find($ma_bc);
+    // Kiểm tra nếu không tìm thấy báo cáo
+    if (!$baocao) {
+        abort(404, 'Báo cáo không tồn tại');
+    }
+    return view('FormVanPhongKhoa.FormQuanLyBaoCaoNghienCuu.FormXemChiTietBaoCao', compact('baocao'));
+    }
+
+    public function phanBienVaBaoVe(){
         return view('FormVanPhongKhoa.FormQuanLyPhanBien.FormBaChucNang');
     }
     public function tintuc()
     {
-        return view('FormVanPhongKhoa.FormTinTuc.FormTinTuc');
+        $vpk = TinTuc::all();
+        return view('FormVanPhongKhoa.FormTinTuc.FormTinTuc',compact('vpk'));
     }
-    public function chiaHoiDong()
-    {
+    public function showTinTuc($ma_tin_tuc){
+        // Lấy dữ liệu tin tức từ database
+        $vpk = TinTuc::find($ma_tin_tuc);
+        // Kiểm tra nếu không tìm thấy tin tức
+        if (!$vpk) {
+            abort(404, 'Tin tức không tồn tại');
+        }
+
+        return view('FormVanPhongKhoa.FormTinTuc.FormDetailTinTuc', compact('vpk'));
+        }
+        public function chiaHoiDong(){
         return view('FormVanPhongKhoa.FormQuanLyPhanBien.FormChiaHoiDong');
-    }
-    public function chonHoiDong()
-    {
-        return view('FormVanPhongKhoa.FormQuanLyPhanBien.FormChonHoiDong');
-    }
+        }
+        public function chonHoiDong(){
+    // Lấy danh sách giảng viên từ database
+        $giangVien = GiangVien::all();
+    // Trả dữ liệu về view
+        return view('FormVanPhongKhoa.FormQuanLyPhanBien.FormChonHoiDong', compact('giangVien'));
+        }
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+        public function create()
+        {
         //
-    }
+        }
 
     /**
      * Store a newly created resource in storage.
