30cc0405a720c86e60d1980383e0bd2e39a064cd
Xanh21807 <anhxuannguyen001@gmail.com>
2025-03-16 16:53:44 +0700

    router web

--- PTDAPM/routes/web.php
+++ PTDAPM/routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaiKhoan\LoginController;
+
 require_once __DIR__ . '/sinhvien.php';
+require_once __DIR__ . '/VPKroute.php';
