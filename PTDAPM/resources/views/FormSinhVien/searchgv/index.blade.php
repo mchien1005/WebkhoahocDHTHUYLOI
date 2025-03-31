25976280e7e1a66b3ee13c5de8effb9f1c10982c
Xanh21807 <167795634+Xanh21807@users.noreply.github.com>
2025-03-31 17:19:53 +0700

    Update index.blade.php

--- PTDAPM/resources/views/FormSinhVien/searchgv/index.blade.php
+++ PTDAPM/resources/views/FormSinhVien/searchgv/index.blade.php
@@ -176,12 +176,12 @@
         </form>
 
         <!-- Thông báo lỗi -->
-        @if(session('success'))
+        <!-- @if(session('success'))
         <div class="alert alert-success alert-dismissible fade show" role="alert">
             {{ session('success') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-        @endif
+        @endif -->
 
         @if(session('error'))
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
